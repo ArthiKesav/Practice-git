648040c653e6d9875a69b5dc68b2ca7f65806247
ArthiKesav <arthinirmala16@gmail.com>
2023-04-14 15:57:29 +0530

    Arthi added new mwthods

---
+++ src/test/java/com/test/Methods.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class Methods {
+private void test1() {
+	System.out.println("1");
+}
+private void test2() {
+	System.out.println("2");
+}
+private void test3() {
+	System.out.println("3");
+}
+}
